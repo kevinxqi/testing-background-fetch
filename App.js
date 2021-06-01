981832d4cae7a529c657ccbf316d276df8131815
Kevin Qi <kevinqi@utexas.edu>
2021-06-01 02:32:53 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, {useEffect} from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as BackgroundFetch from "expo-background-fetch";
+import * as TaskManager from "expo-task-manager";
+
+const TASK_NAME = "BACKGROUND_TASK";
+
+TaskManager.defineTask(TASK_NAME, () => {
+    try {
+        // fetch data here...
+        const receivedNewData = "Simulated fetch " + Math.random();
+        console.log("My task ", receivedNewData);
+        return receivedNewData
+            ? BackgroundFetch.Result.NewData
+            : BackgroundFetch.Result.NoData;
+    } catch (err) {
+        return BackgroundFetch.Result.Failed;
+    }
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    RegisterBackgroundTask = async () => {
+        try {
+            await BackgroundFetch.registerTaskAsync(TASK_NAME, {
+                minimumInterval: 5, // seconds,
+            });
+            console.log("Task registered");
+        } catch (err) {
+            console.log("Task Register failed:", err);
+        }
+    };
+
+    useEffect(() => {
+        RegisterBackgroundTask();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
